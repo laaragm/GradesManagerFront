87930b970f45bf2999ea6a2da5c7d8c2ea7785fb
Lara Galvani <laragalvanim@gmail.com>
2021-06-03 01:37:27 -0300

    added possibility to choose a student to analyze

--- gradesmanagerfront/src/views/StudentPerformance.vue
+++ gradesmanagerfront/src/views/StudentPerformance.vue
@@ -1,14 +1,24 @@
 <template>
 	<div>
-		<v-row no-gutters class="justify-center mt-10" v-for="student in studentsPerformance" :key="student.studentID">
-			<v-col cols=6 class="mt-5">
+		<v-row no-gutters class="justify-center mt-10">
+			<v-col cols=3 class="mt-5">
+				<v-radio-group v-model="studentRadioGroup">
+					<v-radio
+						v-for="student in students" 
+						:key="student.id"
+						:label="student.name"
+						:value="student.id"
+					></v-radio>
+				</v-radio-group>
+			</v-col>
+			<v-col cols=6 class="mt-5 align-center">
 				<v-card elevation=3 class="mt-10">
 					<RadarChart 
 						:title="'Student Performance'" 
-						:values="Object.values(student.gradeAverageByLevel)"
-						:label="student.studentName"
-						:startPoint="getFirstLevel(student)"
-						:lastPoint="getLastLevel(student)"
+						:values="Object.values(selectedStudent.gradeAverageByLevel)"
+						:label="selectedStudent.studentName"
+						:startPoint="getFirstLevel(selectedStudent)"
+						:lastPoint="getLastLevel(selectedStudent)"
 						:color="'#664EAE'"
 						height="500"
 					/>
@@ -19,7 +29,7 @@
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator'
+import { Component, Vue, Watch } from 'vue-property-decorator'
 import { Action } from 'vuex-class'
 import RadarChart from '../components/RadarChart.vue'
 import { Level } from '../enums/Level'
@@ -36,6 +46,15 @@ export default class StudentPerformance extends Vue{
 
 	studentsPerformance = null;
 	students = null;
+	selectedStudent = null;
+	school = 5;
+	studentRadioGroup = null;
+
+	@Watch('studentRadioGroup')
+	onChangeStudent() {
+		console.log(this.studentRadioGroup);
+		this.analyzePerformance(this.studentRadioGroup);
+	}
 
 	getFirstLevel(student: any) {
 		const level = Object.keys(student.gradeAverageByLevel);
@@ -49,12 +68,18 @@ export default class StudentPerformance extends Vue{
 	}
 
 	async mounted() {
+		this.students = await this.getStudents();
+		await this.analyzePerformance(this.students[0].id);
+	}
+
+	async analyzePerformance(id: number) {
 		const studentsDTO = {
-			students: [13,14],
-			school: 5
+			students: [id],
+			school: this.school
 		}
 		this.studentsPerformance = await this.getStudentsPerformance(studentsDTO);
-		this.students = await this.getStudents();
+		this.selectedStudent = this.studentsPerformance[0];
+		this.studentRadioGroup = this.selectedStudent.id;
 	}
 
 }
